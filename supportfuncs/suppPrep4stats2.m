e353df0f6d2f2b132ac1718246b17d37feb4253a
mattansb <35330040+mattansb@users.noreply.github.com>
2018-04-07 17:17:12 +0300

    New support functions for new ERP measuring functions

---
+++ supportfuncs/suppPrep4stats2.m
@@ -0,0 +1,63 @@
+% This function prepares ERP data to be measured by the measurement
+% functions - and is called internaly by them when needed.
+%
+% Author: Mattan S. Ben Shachar, BGU, Israel
+%
+% See also epp_getamplitude& & epp_getamplitude
+
+%{
+Change log:
+-----------
+07-04-2018  New function (written in MATLAB R2015a)
+%}
+
+function [studyOut, T] = suppPrep4stats2(studyIn, conditions, electrodes, timeWindow, ave, jackknife, interpolate)
+
+%% Get only relevant conditions
+% cInd	= cellfun(@(x) find(ismember({studyIn(:).Condition}, x)), conditions);
+cInd    = cellfun(@(x) find(strcmp(x,{studyIn(:).Condition})), conditions);
+studyIn = studyIn(cInd);
+
+for c = 1:length(studyIn)
+    %% Get only relevant electrodes
+    if ave
+        studyIn(c).Data = mean(studyIn(c).Data(electrodes,:,:),1);
+    else
+        studyIn(c).Data = studyIn(c).Data(electrodes,:,:);
+    end
+    
+    %% Jackknife
+    if jackknife
+        studyIn(c).Data = suppJackknife('in',studyIn(c).Data);
+    end
+    
+    %% Interpolate
+    if interpolate~=1 % if any need for interpolation
+        samplingRate_2  = interpolate*1000/(studyIn(c).timeLine(2)-studyIn(c).timeLine(1));
+        
+        timeLine_1      = studyIn(c).timeLine;                                              % old time line
+        timeLine_2      = timeLine_1(1):(1000/samplingRate_2):timeLine_1(end);              % new time line
+        Data_1          = permute(studyIn(c).Data,[2,1,3]);                                 % old data
+        Data_2          = permute(interp1(timeLine_1,Data_1,timeLine_2,'spline'),[2,1,3]);  % new data (interpolated!)
+        
+        studyIn(c).timeLine     = timeLine_2;       % save new time line
+        studyIn(c).Data         = Data_2;           % save new data (interpolated!)
+    end
+    
+    %% Find Time Window
+    T    = dsearchn(studyIn(c).timeLine',timeWindow(1)); % closest point to start of defined time window
+    if length(timeWindow)==2
+        T(2)    = dsearchn(studyIn(c).timeLine',timeWindow(2)); % closest point to end of defined time window
+        T       = T(1):T(2);
+    end
+
+    %% Reduce dims
+    studyIn(c).origSize = size(studyIn(c).Data);
+    studyIn(c).Data = permute(studyIn(c).Data,[2 3 1]);
+    studyIn(c).Data = reshape(studyIn(c).Data,size(studyIn(c).Data,1),[]);
+end
+
+studyOut = studyIn;
+
+
+end
