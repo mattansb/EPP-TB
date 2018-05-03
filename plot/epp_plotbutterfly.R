d988b52ac399cb252345a55630faf4de9782f11c
mattansb <35330040+mattansb@users.noreply.github.com>
2018-05-03 20:39:53 +0300

    minor improvments to R plotting scripts

--- plot/epp_plotbutterfly.R
+++ plot/epp_plotbutterfly.R
@@ -9,13 +9,16 @@ butterfly_plot <- butterfly.data %>%
   ggplot(aes(x = Time, y = amp,
              color = Condition,
              group = ID)) + 
-  facet_wrap(~Condition)
-
-butterfly_plot +
-  geom_vline(xintercept = 0) + # add x-axis at amp = 0
-  geom_hline(yintercept = 0) + # add y-axis lines at time = 0
-  geom_line(alpha = 0.8) + # plot single subject ERPs
-  geom_line(aes(y = meanAmp), color = "black", size = 1) + # plot grand ERPs
+  facet_wrap(~Condition) +
+  ## Axes
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  ## Plot ERPs
+  geom_line(alpha = 0.8) + # single subject ERPs
+  geom_line(aes(y = meanAmp), color = "black", size = 1) + # grand
+  ## Theme, Scales and Labels
   # scale_y_reverse() + # minus up?
-  scale_x_continuous(expand = c(0, 0)) +   # remove padding around time line axis
-  labs(x = "Time", y = expression(paste(mu,"V"))) # add axis labels
+  scale_x_continuous(expand = c(0, 0)) + # remove padding around time line axis
+  labs(x = "Time", y = expression(paste(mu,"V")))
+
+butterfly_plot
