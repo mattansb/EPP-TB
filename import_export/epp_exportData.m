56c4d33df66c9f0bce3d5b7c9d79129119b230f8
mattansb <35330040+mattansb@users.noreply.github.com>
2020-05-09 22:12:23 +0300

    rotate matrix
    and start working on export of ersp / itc

--- import_export/epp_exportData.m
+++ import_export/epp_exportData.m
@@ -35,18 +35,22 @@ Change log:
 function epp_exportData(study,varargin)
 
 %% Validate
+if isfield(study,'ersp') || isfield(study,'itc')
+    n_electrods = size(study(1).ersp, 1);
+    f_range = study(1).freqs;
+else
+    n_electrods = size(study(1).Data, 1);
+    f_range = nan;
+end
+
 p = inputParser;
     addRequired(p,'study',@isstruct);
     addParameter(p,'conditions', {study.Condition}, @iscellstr)
-    addParameter(p,'electrodes', 1:size(study(1).Data, 1), @isnumeric)
+    addParameter(p,'electrodes', 1:n_electrods, @isnumeric)
     addParameter(p,'average', false, @islogical)
     addParameter(p,'timeWindow', study(1).timeLine([1, end]), @isnumeric)
-    % addParameter(p,'freqs', NaN, @isnumeric)
+    addParameter(p,'freqs', f_range, @isnumeric)
 parse(p, study, varargin{:}); % validate
-
-if isfield(study,'ersp') || isfield(study,'itc')
-	error('nope')
-end
 
 
 %% Clean data
@@ -65,21 +69,44 @@ else
 end
 
 for c = 1:length(study)
-    study(c).Data = study(c).Data(p.Results.electrodes,:,:);
-    if p.Results.ave, study(c).Data = mean(study(c).Data, 1); end
+    if isfield(study,'ersp') || isfield(study,'itc')
+        study(c).ersp   = study(c).ersp(p.Results.electrodes,:,:,:);
+        study(c).itc    = abs(study(c).itc(p.Results.electrodes,:,:,:));
+        
+        if p.Results.average, study(c).ersp = mean(study(c).ersp, 1); end
+        if p.Results.average, study(c).itc  = mean(study(c).itc, 1); end
+    else
+        study(c).Data = study(c).Data(p.Results.electrodes,:,:);
+        if p.Results.average, study(c).Data = mean(study(c).Data, 1); end
+    end
 end
 
 
 % timeWindow
 % ----------
 % closest point to start and end of defined time window
-T(1)    = dsearchn(study(1).timeLine',p.Results.timeWindow(1)); 
-T(2)    = dsearchn(study(1).timeLine',p.Results.timeWindow(2));
-T       = T(1):T(2);
+T = dsearchn(study(1).timeLine',p.Results.timeWindow'); 
+T = T(1):T(2);
 t_names = study(1).timeLine(T);
 
 for c = 1:length(study)
-    study(c).Data = study(c).Data(:,T,:);    
+    if isfield(study,'ersp') || isfield(study,'itc')
+        study(c).ersp   = study(c).ersp(:,:,T,:);
+        study(c).itc    = study(c).itc(:,:,T,:);
+    else
+        study(c).Data = study(c).Data(:,T,:);    
+    end
+end
+
+% freqs
+% ----------
+% closest point to start and end of defined time window
+if isfield(study,'ersp') || isfield(study,'itc')
+    F = dsearchn(study(1).freqs',p.Results.freqs'); 
+    f_names = study(1).freqs(F);
+    
+    study(c).ersp = study(c).ersp(:,F,:,:);
+    study(c).itc = study(c).itc(:,F,:,:);
 end
 
 %% Tidy data
@@ -89,23 +116,34 @@ IDs = epp_extractIDs(study, {study.Condition});
 outTable = table();
 
 for c = 1:length(study)
-    % (condition * subject * time) * channels
+    if isfield(study,'ersp') || isfield(study,'itc')
+        error('Can''t do ersp/itc yet')
+        % (condition * subject * time * frequency) * channels (ersp + itc)
+        
+        % id, condition, time and frequency
+        
+        % ersp and itc to array
+        
+        % append
+    else
+        % (condition * subject * time) * channels
     
-    % id, condition, and time
-    xid = repelem(study(c).IDs.ID, length(t_names), 1);
-    xt  = repmat(t_names, 1, length(study(c).IDs.ID))';
-    xc  = repmat({study(c).Condition}, 1, length(xt))';
-    dat = table(xc,xid,xt,'VariableNames',{'Condition', 'ID', 'Time'});
+        % id, condition, and time
+        xid = repelem(study(c).IDs.ID, length(t_names), 1);
+        xt  = repmat(t_names, 1, length(study(c).IDs.ID))';
+        xc  = repmat({study(c).Condition}, 1, length(xt))';
+        dat = table(xc,xid,xt,'VariableNames',{'Condition', 'ID', 'Time'});
     
-    % Data to array
-    eegdat = permute(study(c).Data, [1 3 2]);
-    eegdat = reshape(eegdat,length(e_names),[]);
-    eegdat = array2table(eegdat', 'VariableNames', e_names);
+        % Data to array
+        eegdat = permute(study(c).Data, [1 2 3]);
+        eegdat = reshape(eegdat,length(e_names),[]);
+        eegdat = array2table(eegdat', 'VariableNames', e_names);
     
-    % append
-    dat = [dat, eegdat];
+        % append
+        dat = [dat, eegdat];
     
-    outTable = [outTable; dat];
+        outTable = [outTable; dat];
+    end
 end
 
 outTable = outerjoin(outTable, IDs, 'Keys', {'ID','Condition'}, 'MergeKeys',true);
